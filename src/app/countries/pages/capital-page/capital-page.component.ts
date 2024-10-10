3432821ec11680f6adfeb90df8bf8ba618635862
K3yJey <kevin_madrid10@hotmail.es>
2024-10-09 21:46:15 -0500

    searchbox configured

--- src/app/countries/pages/capital-page/capital-page.component.ts
+++ src/app/countries/pages/capital-page/capital-page.component.ts
@@ -2,7 +2,11 @@ import { Component } from "@angular/core"
 
 @Component({
   selector: "countries-capital-page",
-  templateUrl: "./capital-page.component.html",
-  styles: ``
+  templateUrl: "./capital-page.component.html"
 })
-export class CapitalPageComponent {}
+export class CapitalPageComponent {
+  searchCapital(term: string): void {
+    console.log("SearchCapital")
+    console.log({ term })
+  }
+}
--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,10 +1,17 @@
-import { Component, Input } from "@angular/core"
+import { Component, EventEmitter, Input, Output } from "@angular/core"
 
 @Component({
   selector: "shared-search-box",
   templateUrl: "./search-box.component.html"
 })
 export class SearchBoxComponent {
+  @Output()
+  onValue = new EventEmitter<string>()
+
   @Input()
-  public placeholder: string = ""
+  placeholder: string = ""
+
+  emitValue(term: string): void {
+    this.onValue.emit(term)
+  }
 }
