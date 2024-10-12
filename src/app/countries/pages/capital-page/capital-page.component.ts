37a186b506808d3315f0801f52fd5db848616a66
K3yJey <kevin_madrid10@hotmail.es>
2024-10-11 20:32:21 -0500

    capital query displayed

--- src/app/countries/pages/capital-page/capital-page.component.ts
+++ src/app/countries/pages/capital-page/capital-page.component.ts
@@ -2,17 +2,23 @@ import { Component } from "@angular/core"
 
 import { CountriesService } from "../../services/countries.service"
 
+import { Country } from "../../interfaces/country"
+
 @Component({
   selector: "countries-capital-page",
   templateUrl: "./capital-page.component.html"
 })
 export class CapitalPageComponent {
+  countries: Country[] = []
+
   constructor(private countriesService: CountriesService) {}
 
   searchCapital(term: string): void {
     console.log("SearchCapital")
     console.log({ term })
 
-    this.countriesService.searchCapital(term)
+    this.countriesService.searchCapital(term).subscribe((countries) => {
+      this.countries = countries
+    })
   }
 }
