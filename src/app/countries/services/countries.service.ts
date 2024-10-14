91e3345f82e75b466bbdf09cec9956fc4f6c5178
K3yJey <kevin_madrid10@hotmail.es>
2024-10-14 13:45:19 -0500

    catcherror added in search service

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from "@angular/core"
 import { HttpClient } from "@angular/common/http"
 
-import { Observable } from "rxjs"
+import { catchError, Observable, of } from "rxjs"
 
 import { Country } from "../interfaces/country"
 
@@ -14,6 +14,8 @@ export class CountriesService {
   constructor(private hhtp: HttpClient) {}
 
   searchCapital(term: string): Observable<Country[]> {
-    return this.hhtp.get<Country[]>(`${this.apiUrl}/capital/${term}`)
+    return this.hhtp
+      .get<Country[]>(`${this.apiUrl}/capital/${term}`)
+      .pipe(catchError(() => of([])))
   }
 }
